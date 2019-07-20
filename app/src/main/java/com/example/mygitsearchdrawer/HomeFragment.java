ebaa03b9893d31086fe37893c16948ed68177574
Perpetua Mary <perpetuamary@yahoo.com>
2019-07-21 00:00:57 +0300

    textviews and a button that sets an intent to the about activity

--- app/src/main/java/com/example/mygitsearchdrawer/HomeFragment.java
+++ app/src/main/java/com/example/mygitsearchdrawer/HomeFragment.java
@@ -1,16 +1,34 @@
 package com.example.mygitsearchdrawer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
+    private Button mAboutUsButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         return inflater.inflate(R.layout.fragment_home, container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.about_us).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(),AboutActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
