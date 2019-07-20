03dd977c1b0af6e2c383e29d1321d1b301413424
Perpetua Mary <perpetuamary@yahoo.com>
2019-07-21 01:24:30 +0300

    Add a frequently searched activity. Create a listview and arrayadapters in this activity. Import dependencies necessary to implement Butterknife

--- app/src/main/java/com/example/mygitsearchdrawer/AboutActivity.java
+++ app/src/main/java/com/example/mygitsearchdrawer/AboutActivity.java
@@ -2,13 +2,28 @@ package com.example.mygitsearchdrawer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
+    private Button mFrequentlySearchedButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        mFrequentlySearchedButton =(Button) findViewById(R.id.frequently_searched);
+
+        mFrequentlySearchedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AboutActivity.this, FrequentlySearchedActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/mygitsearchdrawer/FrequentlySearchedActivity.java
+++ app/src/main/java/com/example/mygitsearchdrawer/FrequentlySearchedActivity.java
@@ -2,13 +2,36 @@ package com.example.mygitsearchdrawer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class FrequentlySearchedActivity extends AppCompatActivity {
+    private ListView mListView;
+    private String[] fsearches = new String[]{
+            "perpyotieno","ArnoldOduma","EdithKhalivinzwa","AliceMawia", "NellyWanjiru","ShiroLeah","JoyWambui"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frequently_searched);
+
+        mListView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, fsearches);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String fsearch = ((TextView)view).getText().toString();
+                Toast.makeText(FrequentlySearchedActivity.this, fsearch, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
