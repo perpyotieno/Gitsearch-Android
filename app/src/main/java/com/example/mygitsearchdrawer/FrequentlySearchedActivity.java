59479327f6553559c373d52d8e16a028290c11c4
Perpetua Mary <perpetuamary@yahoo.com>
2019-07-21 00:41:36 +0300

    Add an about us activity

---
+++ app/src/main/java/com/example/mygitsearchdrawer/FrequentlySearchedActivity.java
@@ -0,0 +1,14 @@
+package com.example.mygitsearchdrawer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FrequentlySearchedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frequently_searched);
+    }
+}
