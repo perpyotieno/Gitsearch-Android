6356851f36791710fe227a18a73369113feb87a0
Perpetua Mary <perpetuamary@yahoo.com>
2019-08-13 18:39:05 +0300

    Override the method isLongPressDr5agEnabled() that informs the adapter that drag gestures are enabled

--- app/src/main/java/com/example/mygitsearchdrawer/util/SimpleItemTouchHelperCallback.java
+++ app/src/main/java/com/example/mygitsearchdrawer/util/SimpleItemTouchHelperCallback.java
@@ -1,4 +1,24 @@
 package com.example.mygitsearchdrawer.util;
 
-public class SimpleItemTouchHelperCallback {
+import androidx.recyclerview.widget.ItemTouchHelper;
+
+public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback{
+    private final ItemTouchHelperAdapter mAdapter;
+    //  This constructor takes an ItemTouchHelperAdapter parameter. When implemented in
+    //  FirebaseRestaurantListAdapter, the ItemTouchHelperAdapter instance will pass the gesture event back to the
+    //  Firebase adapter where we will define what occurs when an item is moved or dismissed.
+
+    public SimpleItemTouchHelperCallback(ItemTouchHelperAdapter adapter){
+        mAdapter = adapter;
+    }
+    //  The method below informs the ItemTouchHelperAdapter that drag gestures are enabled.
+    //  We could also disable drag gestures by returning 'false'.
+
+    @Override
+    public boolean isLongPressDragEnabled(){
+        return true;
+    }
+
+
+
 }
