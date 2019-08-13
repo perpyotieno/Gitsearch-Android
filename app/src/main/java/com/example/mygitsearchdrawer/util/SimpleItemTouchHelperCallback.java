889687a8ac47b9e6094f6c402d70269e269c57a6
Perpetua Mary <perpetuamary@yahoo.com>
2019-08-13 18:48:35 +0300

    override the getMovementFlags method that informs the ItemTouchHelper which movement directions are supported.

--- app/src/main/java/com/example/mygitsearchdrawer/util/SimpleItemTouchHelperCallback.java
+++ app/src/main/java/com/example/mygitsearchdrawer/util/SimpleItemTouchHelperCallback.java
@@ -1,6 +1,8 @@
 package com.example.mygitsearchdrawer.util;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback{
     private final ItemTouchHelperAdapter mAdapter;
@@ -18,6 +20,22 @@ public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback{
 
     @Override
     public boolean isItemViewSwipeEnabled() {
+        return true;
+    }
+
+    @Override
+    public int getMovementFlags(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
+        final int dragFlags =ItemTouchHelper.UP | ItemTouchHelper.DOWN;
+        final int swipeFlags = ItemTouchHelper.START | ItemTouchHelper.END;
+        return makeMovementFlags(dragFlags, swipeFlags);
+    }
+
+    @Override
+    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+        if(source.getItemViewType() != target.getItemViewType()) {
+            return false;
+        }
+        mAdapter.onItemMove(source.getAdapterPosition(), target.getAdapterPosition());
         return true;
     }
 }
